f70887bed9097158c29aa8bc5ab1daa4c2a661e9
Anas Ocyber <anas@ocyber.work>
2024-05-16 12:13:50 +0500

    Showing the spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,20 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getOptimizedImages from "../services/get-images";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+
+  if (isLoading) return <Spinner />;
+  if (error) return null;
 
   return (
     <List>
@@ -10,9 +22,10 @@ const GenreList = () => {
         <ListItem key={genre.id}>
           <HStack marginY={2}>
             <Image
-              src={genre.image_background}
+              src={getOptimizedImages(genre.image_background)}
               boxSize="32px"
               borderRadius={3}
+              objectFit="cover"
             />
             <Text fontSize="lg">{genre.name}</Text>
           </HStack>
