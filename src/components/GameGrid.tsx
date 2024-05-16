944f793d6afc00fdf163e48b892e0a6601569c67
Anas Ocyber <anas@ocyber.work>
2024-05-16 11:46:33 +0500

    Build generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import CardSkeleton from "./CardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5];
 
   return (
@@ -18,7 +18,7 @@ const GameGrid = () => {
               <CardSkeleton />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,10 +2,11 @@ import { ListItem, UnorderedList } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useGenres();
+
   return (
     <UnorderedList>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <ListItem key={genre.id}>{genre.name}</ListItem>
       ))}
     </UnorderedList>
